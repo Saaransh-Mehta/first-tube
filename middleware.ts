1df28404de978cac1878ff96d8d70776d067e758
Saaransh-Mehta <saaransh1621m@gmail.com>
2025-05-23 22:31:18 +0530

    first-tube first commit

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { clerkMiddleware , createRouteMatcher} from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+let isPublic = createRouteMatcher([
+    "/signin",
+    "/signup",
+    '/home'
+])
+
+let isPublicApiRoute = createRouteMatcher([
+    "/api/video"
+])
+
+export default clerkMiddleware((auth,req)=>{
+    const {userId}:any = auth;
+    const currentUrl = new URL(req.url) 
+    const isAccessingHome = currentUrl.pathname === '/home'
+    const isAccessingApi = currentUrl.pathname.startsWith('/api')
+
+    if(userId && isPublic(req) && !isAccessingHome){
+        return NextResponse.redirect(new URL('/home',req.url))
+    }
+
+    if(!userId){
+        if(!isPublic(req) && !isPublicApiRoute(req)){
+                return NextResponse.redirect(new URL('/signin',req.url))
+        }
+        
+    }
+    return NextResponse.next()
+
+})
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
